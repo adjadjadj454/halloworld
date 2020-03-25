4ab3c489c4ebc25b8202d099a4eb5e93f3e7210b
adjadjadj454 <61643124+adjadjadj454@users.noreply.github.com>
2020-03-25 22:12:05 +0800

    Update new.py

--- new.py
+++ new.py
@@ -4,4 +4,5 @@ a=input('please tell me your name');
 d=input('this is my first ,please');
 if a and d:
 	print('you are my hero')
-    print('this is passwd',d,sep=':');
+        print('this is passwd',d,sep=':');
+	open('实例.cpp',r);
