fa939d793386136c4c39eb332bb8b1f1c41bc9c2
adjadjadj454 <61643124+adjadjadj454@users.noreply.github.com>
2020-08-15 16:24:31 +0800

    Add files via upload

--- new.py
+++ new.py
@@ -1,8 +1,14 @@
-print('i am feel not good');
-print('bye');
-a=input('please tell me your name');
-d=input('this is my first ,please');
-if a and d:
-	print('you are my hero')
-        print('this is passwd',d,sep=':');
-	open('实例.cpp',r);
+import random
+while True:
+    q=random.randint(0,24)
+    b=random.randint(0,24)
+    c=random.randint(0,24)
+    j=random.randint(0,24)
+    print(q;b;c;j)
+    users=input('please your number')
+    if users=='+' or users=='-' and q+users+b+c+j==24:
+        print('you win')
+        break
+    else:
+        print('youb lose')
+        break
