5a6b120fe0dc1b07d5fafdcc046b106394f85cbf
adjadjadj454 <61643124+adjadjadj454@users.noreply.github.com>
2020-10-06 04:16:06 -0700

    Add files via upload

---
+++ note.py
@@ -0,0 +1,8 @@
+import datetime
+datenow=datetime.datetime.now()
+time=int(input('whe do you want to do it'))
+things=input('what do you want to do')
+while True:
+	if datenow=time:
+		print('you should to'+things)
+
